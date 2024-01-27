178176e3796bccfc06373d04afd088679b4ec543
mahmoudElshimi <mahmoudelshimi@protonmail.ch>
2024-01-27 02:28:41 +0200

    Add Code

---
+++ vec.c
@@ -0,0 +1,156 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* u have to add -lm to linker */
+#include <math.h>
+
+#define DIM(X)        (sizeof X / sizeof X[0])
+
+/* define a vector */
+typedef struct {
+        int dim;
+        double norm;
+        double *arr;
+} Vec;
+
+
+/* define an option that check for error and return a double*/
+typedef struct {
+        int error;
+        char *msg;
+        double result;
+} Option_double;
+
+
+/* define an option that check for error and return a vector */
+typedef struct {
+        int error;
+        char *msg;
+        Vec result;
+} Option_vec;
+
+
+/* get norm of an array of vector */
+double
+get_norm(int dim, double* arr)
+{
+        double norm = 0;
+        for (int i=0; i<dim; i++)
+                norm += (arr[i]*arr[i]);
+        norm = sqrt(norm);
+        return norm;
+
+}
+
+Vec
+new_vec(int dim, double* arr)
+{
+        Vec v = {
+                dim,
+                get_norm(dim, arr),
+		arr 
+        };
+        return v;
+}
+
+
+void
+print_vec(Vec* v, char* name)
+{
+        printf("Vec %s (\nArray:\n", name);
+        for (int i=0; i<v->dim; i++) {
+                printf("\t%f\n", v->arr[i]);
+        }
+	puts("\n");
+        printf("Norm:\t%f\n", v->norm);
+        printf("Dim:\t%d\n", v->dim);
+	puts(")\n");
+}
+
+/* define a dot product on two vectors */
+Option_double
+dot_product(Vec* self, Vec* other)
+{
+        if (self->dim != other->dim) {
+                Option_double ret = {1, "Both dimensions must be equal", 0};
+                return ret;
+        }
+
+        double res = 0;
+        for (int i=0; i<self->dim; i++)
+                res += (self->arr[i] * other->arr[i]);
+
+        Option_double ret = {0, "", res};
+        return ret;
+}
+
+/* define addition on tow vectors */
+Option_vec
+add_vec(Vec* self, Vec* other, double *arr)
+{
+        if (self->dim != other->dim) {
+                Option_vec ret = {1, "Both dimensions must be equal", {}};
+                return ret;
+        }
+	int dim = self->dim;
+        for (int i=0; i<dim; i++)
+                arr[i] = (self->arr[i] + other->arr[i]);
+        Option_vec ret = {0, "", {dim, get_norm(dim, arr), arr}};
+        return ret;
+}
+
+
+/* define substitution on tow vectors */
+Option_vec
+sub_vec(Vec* self, Vec* other, double *arr)
+{
+        if (self->dim != other->dim) {
+                Option_vec ret = {1, "Both dimensions must be equal", {}};
+                return ret;
+        }
+	int dim = self->dim;
+        for (int i=0; i<dim; i++)
+                arr[i] = (self->arr[i] - other->arr[i]);
+        Option_vec ret = {0, "", {dim, get_norm(dim, arr), arr}};
+        return ret;
+}
+
+int
+isorthogonal(Vec* self, Vec* other)
+{
+        Option_double dot = dot_product(self, other);
+        if (dot.error == 0)
+		return dot.result == 0;
+	printf("Err: %s\n", dot.msg);
+	exit(1);	
+}
+
+int
+main()
+{
+        double a[] = {0, 1, 0};
+        double b[] = {0, 0, 1};
+	double c[DIM(a)];
+        Vec v = new_vec(DIM(a), a);
+        Vec w = new_vec(DIM(b), b);
+        print_vec(&v, "V");
+        print_vec(&w, "W");
+	puts("Addition of V+W:");
+        Option_vec u = add_vec(&v, &w, c);
+        if (u.error == 0)
+		print_vec(&u.result, "U");
+        else {
+                printf("Err: %s\n", u.msg);
+		exit(1);	
+	}
+
+        Option_double res = dot_product(&v, &w);
+        if (res.error == 0)
+                printf("Dot Product of V.W: %f\n", res.result );
+        else {
+                printf("Err: %s\n", res.msg);
+		exit(1);
+	}	
+	printf("Are V & W orthogonal? %b\n", isorthogonal(&v, &w));
+	return 0;
+}
---
+++ vec.py
@@ -0,0 +1,68 @@
+from __future__ import annotations
+from typing import Any
+from math import sqrt
+
+
+# define a Vector
+class Vec:
+    def __init__(self: Vec, arr: list[float], name: str = "") -> None:
+        self.arr: list[float] = arr
+        self.dim: int = len(arr)
+        self.norm: float = Vec.get_norm(arr)
+        self.name: str = name
+
+    @staticmethod
+    def get_norm(arr: list[float]) -> float:
+        return sqrt(sum(map(lambda x: x**2, arr)))
+
+    @staticmethod
+    def check_vec(other: Any) -> None:
+        if not isinstance(other, Vec):
+            raise TypeError("Both operands must be of Vec type")
+
+    def __repr__(self: Vec) -> str:
+        return f"Vector {self.name}(\nArray: {self.arr}\nDim: {self.dim}\nNorm:{self.norm}\n)"
+
+    def __str__(self: Vec) -> str:
+        return f"Vector {self.name}(\nArray: {self.arr}\nDim: {self.dim}\nNorm:{self.norm}\n)"
+
+    def __add__(self: Vec, other: Vec) -> Vec:
+        Vec.check_vec(other)
+        return Vec([sum(i) for i in zip(self.arr, other.arr)])
+
+    def __sub__(self: Vec, other: Vec) -> Vec:
+        Vec.check_vec(other)
+        return Vec([i - j for i, j in zip(self.arr, other.arr)])
+
+    def __mul__(self: Vec, other: Vec) -> float:
+        if isinstance(other, Vec):
+            if self.dim != other.dim:
+                raise TypeError(
+                    "unsupported operand type(s) for *: unless both vectors have same dimension"
+                )
+            return sum([i * j for i, j in zip(self.arr, other.arr)])
+        elif isinstance(other, int | float):
+            return Vec.__rmul__(self, other)
+        raise TypeError("Both operands must be of Vec type or scaler * Vec")
+
+    def __rmul__(self: Vec, other: float) -> Vec:
+        return Vec([other * i for i in self.arr])
+
+    def isorthogonal(self: Vec, other: Vec) -> bool:
+        return self * other == 0
+
+
+def main() -> None:
+    v = Vec([0, 1, 0], "V")
+    w = Vec([1, 0, 0], "W")
+    u = v + w
+    u.name = "U"
+    print(v)
+    print(w)
+    print(u)
+    print("Dot product of V.U: ", v * u)
+    print("Are V & W orthogonal? ", v.isorthogonal(w))
+
+
+if __name__ == "__main__":
+    main()
